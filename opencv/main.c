8519118f794ef8f1ee9970f9cd8d43c8d5e2cd52
NaotoNAKATA <nakatanaoto55@gmail.com>
2015-10-02 13:35:34 +0900

    add opencv sample code

---
+++ opencv/main.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include "cv.h"
+#include "highgui.h"
+
+int main(int argc, const char *argv[])
+{
+	IplImage *img = cvLoadImage(argv[1], CV_LOAD_IMAGE_ANYCOLOR | CV_LOAD_IMAGE_ANYDEPTH);
+	cvNamedWindow("main", CV_WINDOW_AUTOSIZE);
+	cvShowImage("main", img);
+	cvWaitKey(0);
+	cvDestroyWindow("lena");
+	cvReleaseImage(&img);
+
+	return 0;
+}
+
