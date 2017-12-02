400ae99b496f6b3bfe7b801131bb303a63c29bef
NaotoNAKATA <nakatanaoto55@gmail.com>
2017-12-02 13:01:44 +0900

    modify python 2.x -> 3.x
    print. and Add with. and " -> '

--- python/main.py
+++ python/main.py
@@ -2,18 +2,25 @@
 
 import sys
 
-if __name__ == "__main__":
+if __name__ == '__main__':
 	param = sys.argv
-	print "Hello:" + param[0]
+	print('Hello:' + param[0])
 
 	# File read
-	f = open(param[0],"r")
+	f = open(param[0],'r')
 	for row in f:
-		print row
+		print(row)
 	f.close()
 
 	#File write
-	f = open("out.txt","w")
-	f.write("Python write file sample")
+	f = open('out.txt','w')
+	f.write('Python write file sample')
 	f.close()
+
+	# with
+	with open(param[0], 'r') as f:
+		for row in f:
+			print(row)
+	with open('out.txt', 'a') as f:
+		f.write('\nadd with\n')
 
