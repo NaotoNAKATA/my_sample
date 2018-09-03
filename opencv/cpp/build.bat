4834057db903b3dc58c4be90d6a610fb4dfb9d96
NaotoNAKATA <nakatanaoto55@gmail.com>
2018-09-03 23:37:51 +0900

    Modify cpp build.bat

--- opencv/cpp/build.bat
+++ opencv/cpp/build.bat
@@ -7,9 +7,15 @@ set PATH=%PATH%;C:\Program Files (x86)\MSBuild\14.0\Bin
 set VisualStudio="Visual Studio 14 Win64"
 set OpenCV="C:\opencv\opencv-3.2.0\build"
 
-if not exist build (
-	mkdir build
-	cd build
+set BUILD=build
+set PRJ=ProjectMain.sln
+set OBJ=main.exe
+set CONFIG=Release
+set PLAT=x64
+
+if not exist %BUILD% (
+	mkdir %BUILD%
+	cd %BUILD%
 	cmake .. -G %VisualStudio% -DCMAKE_PREFIX_PATH=%OpenCV%
 	cd ..
 )
@@ -18,14 +24,14 @@ echo.
 set /P STR="(1/Build : 2/Rebuild : 3/Clean) : "
 
 if %STR%==1 (
-	MSBuild.exe build\ProjectMain.sln /t:Build /p:Configuration=Release /p:platform=x64
-	xcopy /Y build\Release\main.exe
+	MSBuild.exe %BUILD%\%PRJ% /t:Build /p:Configuration=%CONFIG% /p:platform=%PLAT%
+	xcopy /Y %BUILD%\%CONFIG%\%OBJ%
 ) else if %STR%==2 (
-	MSBuild.exe build\ProjectMain.sln /t:Rebuild /p:Configuration=Release /p:platform=x64
-	xcopy /Y build\Release\main.exe
+	MSBuild.exe %BUILD%\%PRJ% /t:Rebuild /p:Configuration=%CONFIG% /p:platform=%PLAT%
+	xcopy /Y %BUILD%\%CONFIG%\%OBJ%
 ) else if %STR%==3 (
-	if exist main.exe ( del /Q main.exe)
-	MSBuild.exe build\ProjectMain.sln /t:Clean /p:Configuration=Release /p:platform=x64
+	if exist %OBJ% ( del /Q %OBJ%)
+	MSBuild.exe %BUILD%\%PRJ% /t:Clean /p:Configuration=%CONFIG% /p:platform=%PLAT%
 ) else (
 	echo %STR%" : No Action"
 )
