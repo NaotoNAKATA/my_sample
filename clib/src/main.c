f619d7ec08e6bba68f16213bac24bd1f22c49f67
NaotoNAKATA <nakatanaoto55@gmail.com>
2019-02-16 00:14:09 +0900

    clibs kari

---
+++ clib/src/main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(int argc, const char *argv[])
+{
+	printf("Hello\n");
+	// 静的リンク(同一のプロジェクト)
+	// 動的リンク(同一のプロジェクト)
+	// 静的リンク(別々のプロジェクト)
+	// 動的リンク(別々のプロジェクト)
+
+	return 0;
+}
