9819f7415e750ce9f89f77695672c830f126c887
NaotoNAKATA <nakatanaoto55@gmail.com>
2020-05-12 15:02:02 +0900

    bugfix and add sample

--- gtest/gtest_sample/gtest_sample.cpp
+++ gtest/gtest_sample/gtest_sample.cpp
@@ -1,1 +1,17 @@
-#include "gtest/gtest"
+#include "gtest/gtest.h"
+
+class TestClass01 : public ::testing::Test {
+	
+	// 各テストの直前に呼ばれる
+	virtual void SetUp() {
+	}
+	
+	// 各テストの直後に呼ばれる
+	virtual void TearDown() {
+	}
+};
+
+TEST_F(TestClass01, TestCase01)
+{
+	EXPECT_FLOAT_EQ(0.0f, 0.0f);
+}
--- gtest/install.sh
+++ gtest/install.sh
@@ -3,8 +3,8 @@
 VER=1.10.0
 #wget https://github.com/googletest/archive/release-${VER}.zip
 
-unzip googletest-release-${VER}.zip
-cd googletest-release-${VER}.zip
+#unzip googletest-release-${VER}.zip
+cd googletest-release-${VER}
 
 BUILD=build
 mkdir ${BUILD}
