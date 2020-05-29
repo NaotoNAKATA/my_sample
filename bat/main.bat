5cad5a3254998bd51945017c449a31507d939e83
NaotoNAKATA <nakatanaoto55@gmail.com>
2020-05-29 09:32:21 +0900

    mod bat

--- bat/main.bat
+++ bat/main.bat
@@ -8,6 +8,18 @@ echo %~x0
 
 cd %~dp0
 
+SET CSV=%1
+
+SET FILT="[0:v]setpts=PTS-STARTS,scale=640x480[v0];[1:v]setpts=PTS-STARTS,scale=640x480[v1];[v0][v1]hstack"
+
+for /f "delims=',', tokens=1-3" %%a in (%CSV%) do (
+	SET OUT=%%a
+	SET COL2=%%b
+	SET COL3=%%c
+	
+rem	ffmpeg.exe -i !COL2! -i !COL3! -filter_complex !FILT! -r 10 -vb 3000k !OUT!
+)
+
 pause
 
 exit /b
