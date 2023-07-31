923c59edf0eceed7696876b853af5b634bcdba0f
NaotoNAKATA <nakatanaoto55@gmail.com>
2023-07-31 13:03:39 +0900

    mod

--- python/img/ss_eve.py
+++ python/img/ss_eve.py
@@ -7,22 +7,21 @@ import glob
 if __name__ == '__main__':
 	# テスト
 	#root_dir = 'C:\Users\NAKATANAOTO\work\game\EVE_burst_error\Dec.2\01-まりな編\'
-	p_list = (
-		#root_dir + '01-本部',
-		#root_dir + '02-403号室',
-		#root_dir + '03-本部',
-		#root_dir + '04-403号室',
-		#root_dir + '05-外国人学校',
-		#root_dir + '06-本部',
-		#root_dir + '07-403号室',
-                #'01-外国人学校前',
-                '12-ディレクタールーム',
-	)
+	#p_list = (
+	#	#root_dir + '01-本部',
+	#	#root_dir + '02-403号室',
+	#	#root_dir + '03-本部',
+	#	#root_dir + '04-403号室',
+	#	#root_dir + '05-外国人学校',
+	#	#root_dir + '06-本部',
+	#	#root_dir + '07-403号室',
+        #        #'01-外国人学校前',
+        #        '12-ディレクタールーム',
+	#)
 	
+	root_dir = './??-*'
+	p_list = sorted(glob.glob(root_dir))
 	for p in p_list:
-		ss_doc(p).run()
-
-        #print(sorted(glob.glob( './')))
-
-
+		p0 = p.replace('.\\','')
+		ss_doc(p0).run()
 
