cb844ebf57b3d5f9d840496fa0434ccb91b7634c
NaotoNAKATA <nakatanaoto55@gmail.com>
2024-01-24 00:24:48 +0900

    refact

--- numbers/num_solver.py
+++ numbers/num_solver.py
@@ -34,48 +34,42 @@ class num_solver:
 		# グループ間の重なり
 		self.make_overlap()
 		
-		# (特殊)ジョイント
-		for joint_list in te['joint']:
-			self.make_joint(joint_list, self.len)
-		
-		# (特殊)ジョイントの反転
-		for joint_list in te['njoint']:
-			self.make_njoint(joint_list, self.len)
-		
-		# (特殊)不等号
-		for ineq_list in te['inequal']:
-			self.make_ineq(ineq_list, self.len)
+		# 特殊グループ
+		special = [
+			['joint', num_joint, self.len], # (特殊)ジョイント
+			['njoint', num_njoint, self.len], # (特殊)ジョイントの反転
+			['inequal', num_ineq, self.len], # (特殊)不等号
+			['arrow', num_arrow, None], # (特殊)合計アロー
+			['mult', num_mult, None], # (特殊)九九
+		]
+		for [key, ng, arg] in special:
+			self.make_group(te[key], ng, arg)
 			
 		# (特殊)足し算
 		for s, sums_list in te['sums']:
 			self.make_sums(sums_list, s)
-		
-		# (特殊)合計アロー
-		for arrow_list in te['arrow']:
-			self.make_arr(arrow_list)
 			
 		# (特殊)偶数
 		for evn_list in te['even']:
 			self.del_odd( evn_list )
-			
-		# (特殊)九九
-		for mm_list in te['mult']:
-			self.make_mult(mm_list)
 	
 	def set(self, _idx, _num=-1):
 		""" 数字クラスの登録 """
 		self.nb[_idx] = num_box(
 				_len=self.len, _num=_num)
 	
-	def make_group(self, group_list, ng):
+	def make_group(self, group_list, ng, arg=None):
 		""" グループの作成 """
 		for group in group_list:
 			num_box_list = []
 			for idx in group:
 				num_box_list.append( self.nb[idx] )
 			
-			self.grp.append( ng(num_box_list) )
-	
+			if arg==None:
+				self.grp.append( ng(num_box_list) )
+			else:
+				self.grp.append( ng(num_box_list, arg) )
+				
 	def make_overlap(self):
 		""" 重なりをチェック """
 		l = len(self.grp)
@@ -84,30 +78,6 @@ class num_solver:
 				no = num_overlap( self.grp[i], self.grp[j] )
 				if no.is_overlap():
 					self.grp.append( no )
-		
-	def make_joint(self, joint_list, _len):
-		""" (特殊)ジョイントクロス """
-		num_box_list = []
-		for idx in joint_list:
-			num_box_list.append( self.nb[idx])
-			
-		self.grp.append( num_joint(num_box_list, _len) )
-	
-	def make_njoint(self, joint_list, _len):
-		""" (特殊)ジョイントクロスの反転 """
-		num_box_list = []
-		for idx in joint_list:
-			num_box_list.append( self.nb[idx])
-			
-		self.grp.append( num_njoint(num_box_list, _len) )
-		
-	def make_ineq(self, ineq_list, _len):
-		""" (特殊)不等号 """
-		num_box_list = []
-		for idx in ineq_list:
-			num_box_list.append( self.nb[idx])
-			
-		self.grp.append( num_ineq(num_box_list, _len) )
 	
 	def make_sums(self, sums_list, s):
 		""" (特殊)足し算 """
@@ -117,27 +87,11 @@ class num_solver:
 		
 		self.grp.append( num_sum(num_box_list, s) )
 	
-	def make_arr(self, arr_list):
-		""" (特殊)合計アロー """
-		num_box_list = []
-		for idx in arr_list:
-			num_box_list.append( self.nb[idx])
-			
-		self.grp.append( num_arrow(num_box_list) )
-	
 	def del_odd(self, evn_list):
 		""" 奇数を削除 """
 		for idx in evn_list:
-			self.nb[idx] .del_cand_odd()
-	
-	def make_mult(self, mm_list):
-		""" (特殊)九九 """
-		num_box_list = []
-		for idx in mm_list:
-			num_box_list.append( self.nb[idx])
-			
-		self.grp.append( num_mult(num_box_list) )
-	
+			self.nb[idx].del_cand_odd()
+		
 	def is_ok(self):
 		""" クラス内の数字がすべて確定していれば True """
 		return all( map(lambda x:x.is_ok(), self.nb.values()) )
