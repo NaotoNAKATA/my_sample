bd44e047bc41e620f8c4edc91bdce9e795821657
NaotoNAKATA <nakatanaoto55@gmail.com>
2024-07-20 10:53:00 +0900

    mod

--- python/fe/fe_param_00/prologue_scene.py
+++ python/fe/fe_param_00/prologue_scene.py
@@ -1,16 +1,12 @@
-#base_dir = '/storage/43E3-332A/others/test/work/ROM/SNES/ファイアーエムブレム 聖戦の系譜 (J)/'
-base_dir = './test/'
+base_dir = '/storage/43E3-332A/others/test/work/ROM/SNES/ファイアーエムブレム 聖戦の系譜 (J)/'
+#base_dir = './test/'
 chaper_dir = '00-序章-聖騎士誕生/'
 scene_dir = '00-プロローグ/'
-
-out_file = '序章-聖騎士誕生-プロローグ.gif'
-fps = 0.5
 
 dirs = [
 	base_dir + chaper_dir + scene_dir,
 ]
 
-"""
 
 phrase = [ 
 	[0,[
@@ -144,4 +140,4 @@ phrase = [
 ]
 
 title_file = base_dir + '00-序章-聖騎士誕生/01-シアルフィ出撃/2024-06-07 午後11.32.25.png'
-"""
+
---
+++ python/fe/fe_prologue_scene.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+
+from fe_pil import fe_pil
+from fe_prologue import fe_prologue
+	
+class fe_prologue_scene(fe_prologue):
+	""" プロローグクラス(スライド) """
+	def save(self):
+		""" メイン画像の保存 """
+		for i, fe in enumerate(self.fe):
+			filename = self.TEMP_DIR + '/{0:0>2}.png'.format(i)
+			fe.save(filename)
+		
+	def run(self):
+		""" 実行 """
+		self.del_temp()
+		self.read()
+		self.save_main_temp()
+		
+if __name__ == '__main__':
+	
+	import fe_param_00.prologue_scene as p
+	
+	fe = fe_prologue_scene()
+	fe.configure(p)
+	fe.run()
+	
