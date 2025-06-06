cc2477d08eb8092e839a2a4eb40d76eab2d2f315
NaotoNAKATA <nakatanaoto55@gmail.com>
2025-06-06 18:51:31 +0900

    mod

---
+++ python/fe/fe_param_04/prologue_map.py
@@ -0,0 +1,19 @@
+from .params import *
+scene_dir = [
+	'00-プロローグ/',
+	'00-プロローグ追加画像/',
+]
+dirs = [ base_dir + chaper_dir + s for s in scene_dir]
+#files = []
+
+out_file = '第四章-空に舞う-プロローグ.png'
+
+TEST_RUN_FIRST = False
+TEST_RUN = True
+RUN = False
+
+comp = [
+		[15, (0,0)],
+		[21, (80,0)],
+	]
+
--- python/fe/fe_param_a/field00.py
+++ python/fe/fe_param_a/field00.py
@@ -4,11 +4,13 @@ chaper_dir00 = '00-序章-聖騎士誕生/'
 chaper_dir01 = '01-第一章-精霊の森の少女/'
 chaper_dir02 = '02-第二章-アグストリアの動乱/'
 chaper_dir03 = '03-第三章-獅子王エルトシャン/'
+chaper_dir04 = '04-第四章-空に舞う/'
 
 scene_file00 = '序章-聖戦士誕生-フィールド.png'
 scene_file01 = '第一章-精霊の森の少女-フィールド.png'
 scene_file02 = '第二章-アグストリアの動乱-フィールド.png'
 scene_file03 = '第三章-獅子王エルトシャン-フィールド.png'
+scene_file04 = '第四章-空に舞う-フィールド.png'
 
 
 dirs = [
@@ -18,8 +20,9 @@ files = [
 	base_dir + chaper_dir00 + scene_file00,
 	base_dir + chaper_dir01 + scene_file01,
 	base_dir + chaper_dir02 + scene_file02,
-	#base_dir + chaper_dir02 + scene_file03,
-	'temp2/field_66.png',
+	base_dir + chaper_dir03 + scene_file03,
+	base_dir + chaper_dir04 + scene_file04,
+	
 ]
 
 out_file = '全体フィールド.png'
@@ -36,4 +39,7 @@ comp = [
 		[2, (45-56,15-61)],
 		#[3, (52-35,8-61)],
 		[3, (52-35,8-61-1)],
+		
+		# TBD
+		[4, (90,0)],
 	]
